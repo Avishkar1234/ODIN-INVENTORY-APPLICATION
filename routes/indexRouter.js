ec776575cd5264e10c6323d9d4c4344097b683a0
Avishkar <avishkar1234567890@gmail.com>
2026-06-10 21:34:48 +0530

    Debug homepage rendering

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../db/pool");
 
-router.get("/", (req, res) => {
-  res.send("HOME ROUTE WORKS");
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM categories");
+
+    return res.render("category_form", {
+      categories: result.rows,
+    });
+  } catch (err) {
+    return res.status(500).send(`<pre>${err.stack}</pre>`);
+  }
 });
 
 module.exports = router;
